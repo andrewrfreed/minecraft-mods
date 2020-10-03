91d2bbe10b122405bb5a553e3b326317eabeacb8
Andrew R. Freed <arfreed@nc.rr.com>
2020-10-03 13:04:47 -0400

    add copper item from tutorial

--- test-mod-1.16.3/src/main/java/com/freedville/minecraft/mods/FreedvilleExploreMod.java
+++ test-mod-1.16.3/src/main/java/com/freedville/minecraft/mods/FreedvilleExploreMod.java
@@ -2,6 +2,7 @@ package com.freedville.minecraft.mods;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.material.Material;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -19,9 +20,11 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("freedville_explore_mod")
+@Mod(FreedvilleExploreMod.MODID)
 public class FreedvilleExploreMod
 {
+	public static final String MODID = "freedville_explore_mod";
+	
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
@@ -37,6 +40,8 @@ public class FreedvilleExploreMod
 
         // Register ourselves for server and other game events we are interested in
         MinecraftForge.EVENT_BUS.register(this);
+        
+        RegistryHandler.init();
     }
 
     private void setup(final FMLCommonSetupEvent event)
---
+++ test-mod-1.16.3/src/main/java/com/freedville/minecraft/mods/ModBlock.java
@@ -0,0 +1,17 @@
+package com.freedville.minecraft.mods;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockRenderType;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.material.Material;
+
+public class ModBlock extends Block {
+	final String MODID = "OurMod";
+
+	// https://github.com/TheGreyGhost/MinecraftByExample/blob/master/src/main/java/minecraftbyexample/mbe01_block_simple/BlockSimple.java
+	public ModBlock() {
+		//// look at Block.Properties for further options
+		// typically useful: hardnessAndResistance(), harvestLevel(), harvestTool()
+		super(Block.Properties.create(Material.ROCK));
+	}
+}
